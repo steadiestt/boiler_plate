cac41a11786b2385c5832f700654cc27dc4dd0a7
steadiestt <dlrnak158@naver.com>
2022-05-29 18:42:40 +0900

    비밀 정보 보호

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const app = express()
+const port = 5000
+const bodyParser = require('body-parser');
+
+const config = require('./config/key');
+
+const { User } = require("./models/User");
+
+// 두 타입의 정보를 가져오기 위함
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+// application/json
+app.use(bodyParser.json());
+
+const mongoose = require('mongoose')
+mongoose.connect(config.mongoURI, {
+  useNewUrlParser: true, useUnifiedTopology: true
+}).then(() => console.log('MongoDB Connected..'))
+  .catch(err => console.log(err))
+
+app.get('/', (req, res) => {
+  res.send('hi :)')
+})
+
+app.post('/register', (req, res) => {
+  // 회원가입할 때 필요한 정보를 client에서 가져오면
+  // 그것들을 DB에 넣어준다.
+  const user = new User(req.body)
+
+  user.save((err, doc) => {
+    if(err) return res.json({success: false, err})
+    return res.status(200).json({
+      success: true
+    })
+  })
+})
+
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`)
+})
