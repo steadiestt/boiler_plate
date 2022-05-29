427e25c53f8133d01baed20e6bf09a41dac986c1
steadiestt <dlrnak158@naver.com>
2022-05-29 18:59:25 +0900

    Hi :)

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ mongoose.connect(config.mongoURI, {
   .catch(err => console.log(err))
 
 app.get('/', (req, res) => {
-  res.send('bye :(')
+  res.send('Hi :)')
 })
 
 app.post('/register', (req, res) => {
